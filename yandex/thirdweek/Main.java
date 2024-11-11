80951de09507e92999299bafc7020faa593220d3
slava <slava_ponomarev_)5@mail.ru>
2024-11-11 16:11:36 +0300

    write solution of find near right min

---
+++ yandex/thirdweek/Main.java
@@ -0,0 +1,10 @@
+package thirdweek;
+
+import static thirdweek.FindNearMinRight.findNearMinRight;
+
+public class Main {
+    public static void main(String[] args) {
+            findNearMinRight();
+
+    }
+}

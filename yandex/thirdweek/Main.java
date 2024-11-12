2222a7d4cdeae0e10ad48a91a730387bb8b2d6c6
Пономарев Вячеслав Игоревич <148494872+slavappppv@users.noreply.github.com>
2024-11-12 12:26:39 +0300

    Delete yandex/thirdweek/Main.java

--- yandex/thirdweek/Main.java
+++
@@ -1,9 +0,0 @@
-package thirdweek;
-
-import static thirdweek.FindMinOnSegment.findMinOnSegment;
-
-public class Main {
-    public static void main (String[] args){
-        findMinOnSegment();
-    }
-}

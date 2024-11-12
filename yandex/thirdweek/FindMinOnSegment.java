794a1491ec57e1b4ee34ee33391785b09ee3570c
slava <slava_ponomarev_)5@mail.ru>
2024-11-12 12:23:12 +0300

    work version of find min on the segment

---
+++ yandex/thirdweek/FindMinOnSegment.java
@@ -0,0 +1,36 @@
+package thirdweek;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+
+public class FindMinOnSegment {
+    public static void findMinOnSegment() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            String[] lens = reader.readLine().split(" ");
+            ArrayDeque<Integer> dequeOfNums = new ArrayDeque<Integer>();
+            String[] nums = reader.readLine().split(" ");
+            for (int i = 0; i < Integer.parseInt(lens[0]); ++i) {
+                if (dequeOfNums.isEmpty()) {
+                    dequeOfNums.addFirst(Integer.parseInt(nums[i]));
+                }
+                else {
+                    while (!(dequeOfNums.isEmpty()) && (Integer.parseInt(nums[i]) < dequeOfNums.getFirst())) {
+                        dequeOfNums.pop();
+                    }
+                    dequeOfNums.addFirst(Integer.parseInt(nums[i]));
+                }
+                if (i >= (Integer.parseInt(lens[1]) - 1)) {
+                    System.out.println(dequeOfNums.getLast());
+                    if (dequeOfNums.getLast() == Integer.parseInt(nums[i - (Integer.parseInt(lens[1]) - 1)])) {
+                        dequeOfNums.removeLast();
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ yandex/thirdweek/Main.java
@@ -0,0 +1,9 @@
+package thirdweek;
+
+import static thirdweek.FindMinOnSegment.findMinOnSegment;
+
+public class Main {
+    public static void main (String[] args){
+        findMinOnSegment();
+    }
+}
